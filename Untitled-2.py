bfdd03f50eed415bd7dc2a4eb8369d3fefe9372f
S-E0K <rqt6238@naver.com>
2024-11-12 19:52:55 +0900

    asdf

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,2 +1,10 @@
-a = 10
-print(a)
+import heapq
+
+# def replacement_selection(cases, buffer_size = 5) : 
+    
+
+
+
+test_cases = read_input("replacement_input.txt")
+results = replacement_selection(test_cases)
+write_output(results, "replacement_output.txt")
