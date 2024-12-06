c65e20d9729b6473078f7b6f1114d90f08926b4e
S-E0K <rqt6238@naver.com>
2024-12-06 21:32:46 +0900

    asdf

--- BST_2024174.py
+++ BST_2024174.py
@@ -3,6 +3,10 @@
 
 
 
+
+
+
+
 class TreeNode:
     def __init__(self, newItem, left = None, right = None) :
         self.item = newItem
@@ -18,56 +22,55 @@ class BinarySearchTree :
 
     def __searchItem(self, tNode: TreeNode, x, path: str) -> str :
         if (tNode == None) :
-            return path  # 노드 없을 때
+            return path
         elif (x == tNode.item) :
-            return path  # 키 찾으면
+            return path
         elif (x < tNode.item) :
-            return self.__searchItem(tNode.left, x, path + "0")  # 값이 작으면 왼쪽
+            return self.__searchItem(tNode.left, x, path + "0")
         else :
-            return self.__searchItem(tNode.right, x, path + "1")  # 값이 크면 오른쪽
+            return self.__searchItem(tNode.right, x, path + "1")
 
     def insert(self, newItem) :
         self.__root = self.__insertItem(self.__root, newItem)
 
     def __insertItem(self, tNode: TreeNode, newItem) -> TreeNode :
         if (tNode == None) :
-            tNode = TreeNode(newItem)  # 노드가 없으면
+            tNode = TreeNode(newItem)
         elif (newItem < tNode.item) :
-            tNode.left = self.__insertItem(tNode.left, newItem)  # 왼쪽 서브트리에 삽입
+            tNode.left = self.__insertItem(tNode.left, newItem)
         elif (newItem > tNode.item) :
-            tNode.right = self.__insertItem(tNode.right, newItem)  # 오른쪽 서브트리에 삽입
-        return tNode  # 수정된 트리 노드 반환
-
+            tNode.right = self.__insertItem(tNode.right, newItem)
+        return tNode
     def delete(self, x) :
         self.__root = self.__deleteItem(self.__root, x)
 
     def __deleteItem(self, tNode: TreeNode, x) -> TreeNode :
         if (tNode == None) :
-            return None  # 없을 때
+            return None
         elif (x == tNode.item) :
-            tNode = self.__deleteNode(tNode)  # 있을 때
+            tNode = self.__deleteNode(tNode)
         elif (x < tNode.item) :
-            tNode.left = self.__deleteItem(tNode.left, x)  # 왼쪽 서브트리에서 삭제
+            tNode.left = self.__deleteItem(tNode.left, x)
         else :
-            tNode.right = self.__deleteItem(tNode.right, x)  # 오른쪽 서브트리에서 삭제
-        return tNode  # 수정된 트리 노드 반환
+            tNode.right = self.__deleteItem(tNode.right, x)
+        return tNode
 
     def __deleteNode(self, tNode: TreeNode) -> TreeNode :
         if ((tNode.left == None) and (tNode.right == None)) :
-            return None  # 자식이 없음
+            return None
         elif (tNode.left == None) :
-            return tNode.right  # 왼쪽 자식이 없음
+            return tNode.right
         elif (tNode.right == None):
-            return tNode.left  # 오른쪽 자식이 없음
+            return tNode.left
         else :
-            (rtnItem, rtnNode) = self.__deleteMinItem(tNode.right) # 둘 다 있음
+            (rtnItem, rtnNode) = self.__deleteMinItem(tNode.right)
             tNode.item = rtnItem
             tNode.right = rtnNode
             return tNode
 
     def __deleteMinItem(self, tNode: TreeNode) -> tuple :
         if (tNode.left == None) :
-            return (tNode.item, tNode.right)  # 노드와 오른쪽 자식을 반환
+            return (tNode.item, tNode.right)
         else :
             (rtnItem, rtnNode) = self.__deleteMinItem(tNode.left)
             tNode.left = rtnNode
@@ -80,68 +83,67 @@ class BinarySearchTree :
         self.__root = None
 
 
-def process_input_output(readFile, writeFile) : # 서치
+def process_input_output(readFile, writeFile)
     lines = readFile.readlines()
     line = 0
-    t = int(lines[line].strip())  # 테스트 케이스 개수
+    t = int(lines[line].strip())
     line += 1
 
     for j in range(t) :
         bst = BinarySearchTree()
 
 
-        i = int(lines[line].strip())  # 삽입할 키의 개수
+        i = int(lines[line].strip())
         line += 1
 
-        keys = lines[line].strip().split()  # 문자열을 공백을 기준으로 나누어 리스트로 변환
+        keys = lines[line].strip().split()
 
         for j in range(i) :
-            key = int(keys[j])  # 문자열 요소를 정수로 변환
-            bst.insert(key)  # 정수로 변환된 키를 트리에 삽입
+            key = int(keys[j])
+            bst.insert(key)
 
         line += 1
 
 
-        s1 = int(lines[line].strip())  # 삭제 전 검색할 키의 개수
+        s1 = int(lines[line].strip())
         line += 1
 
-        keys = lines[line].strip().split()  # 현재 줄의 내용을 읽고 공백을 기준으로 나누어 리스트로 변환
+        keys = lines[line].strip().split()
 
         for j in range(s1) :
-            key = int(keys[j])  # 문자열 요소를 정수로 변환
-            path = bst.search(key)  # BST에서 해당 키를 검색
-            writeFile.write(path + "\n")  # 검색 경로를 출력 파일에 기록
+            key = int(keys[j])
+            path = bst.search(key)
+            writeFile.write(path + "\n")
 
         line += 1
 
 
-        d = int(lines[line].strip())  # 삭제할 키의 개수
+        d = int(lines[line].strip())
         line += 1
 
-        keys = lines[line].strip().split()  # 현재 줄의 내용을 읽고 공백을 기준으로 나누어 리스트로 변환
+        keys = lines[line].strip().split()
 
         for j in range(d):
-            key = int(keys[j])  # 문자열 요소를 정수로 변환
-            bst.delete(key)  # 정수로 변환된 키를 트리에서 삭제
+            key = int(keys[j])
+            bst.delete(key)
 
         line += 1
 
 
-        s2 = int(lines[line].strip())  # 삭제 후 검색할 키의 개수
+        s2 = int(lines[line].strip())
         line += 1
 
         line_content = lines[line].strip()
-        keys = line_content.split()  # 현재 줄의 내용을 읽고 공백을 기준으로 나누어 리스트로 변환
+        keys = line_content.split()
 
         for index in range(s2):
-            key = int(keys[index])  # 문자열 요소를 정수로 변환
-            path = bst.search(key)  # BST에서 해당 키를 검색
-            writeFile.write(path + "\n")  # 검색 경로를 출력 파일에 기록
+            key = int(keys[index])
+            path = bst.search(key)
+            writeFile.write(path + "\n")
 
         line += 1
 
 
-# 실행 부분
 readFile = open('bst_input.txt', 'r')
 writeFile = open('bst_output.txt', 'w')
 process_input_output(readFile, writeFile)
