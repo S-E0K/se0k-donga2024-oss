59c0b720816813b8dc31c17528ee989c2d579b4b
보석 김 <2024174@donga.ac.kr>
2025-04-04 13:40:21 +0900

    a

--- Untitled-1.py
+++ Untitled-1.py
@@ -4104,14 +4104,22 @@ connect.close()
 
 
 # %%
+## 과제 됨
 
-
-
-
-
-
-
-
+import pymysql
+connect = pymysql.connect(host='127.0.0.1', user='root',
+password='1234', db='market_db', charset='utf8')
+cursor = connect.cursor()
+cursor.execute("SELECT * FROM member")
+print("사용자ID 사용자이름 이메일 가입연도")
+print("---------------------------------------------------------")
+while (True) :
+    row = cursor.fetchone() # select 해 온 각 행(튜플)
+    if row== None :
+        break
+    print("%-3s %-6s %-2d %-10s %-10s %-10s %d %-10s" % 
+          (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
+connect.close()
 
 
 
